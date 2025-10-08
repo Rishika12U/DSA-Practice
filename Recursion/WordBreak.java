01567c443e2cbe3b7cc658133fdf169054504b93
Rishika Urmaliya <151054846+Rishika12U@users.noreply.github.com>
2025-10-08 21:39:23 +0530

    Create WordBreak.java

---
+++ Recursion/WordBreak.java
@@ -0,0 +1,19 @@
+class Solution {
+public boolean helper(String s, Set<String>set){
+    int len = s.length();
+    if(len == 0)return true;
+
+    for(int i=1; i<=len; i++){
+        if(set.contains(s.substring(0, i)) && helper(s.substring(i), set)){
+            return true;
+        }
+    }
+    return false;
+}
+
+    public boolean wordBreak(String s, List<String> wordDict) {
+      Set<String>set = new HashSet<>(wordDict);
+
+      return helper(s, set);
+    }
+}
