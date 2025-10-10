9dec081be6cb720a9ab13e57daaca9b1137606d3
Rishika Urmaliya <151054846+Rishika12U@users.noreply.github.com>
2025-10-10 13:40:13 +0530

    Create SwapTwoNo.java

---
+++ BitManipulation/SwapTwoNo.java
@@ -0,0 +1,11 @@
+ public int[] swap(int a, int b) {
+        
+        int[] ans = new int[2];
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        ans[0] = a;
+        ans[1] = b;
+        return ans;
+    }
+}
